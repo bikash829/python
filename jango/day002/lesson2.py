784d5b1f7d8ec1d71683fd111a4309668051a4af
bikash829 <chowdhurybikash38@gmail.com>
2020-02-18 11:13:08 +0600

    something

---
+++ jango/day002/lesson2.py
@@ -0,0 +1,23 @@
+a = 5
+b = 3.2
+c = "hello"
+
+print(a)
+print(b)
+print(type(c))
+
+p,q,r = 56,34.2,"HI"
+
+print(p,q,r)
+
+print("p = %s and q = %s and r = %s"%(p,q,r))
+print("p={0} and q={1} and r={2}".format(p,q,r))
+print("p={} and q={} and r={}".format(p,q,r))
+print("p="+str(p)+" and q ="+'q = '+str(q)+' and r = '+r+'.')
+print("We love {}".format("akdhfiadshfif"))
+
+
+age = 123
+message = "Happy Basins " + str(age) + "th Birthday"
+
+print(message)
